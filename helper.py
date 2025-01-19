9f4ae7323cc754cfd88552d446ca2f89d4a6968e
Yaroslav Ponomarenko <ya.ponomarenko@me.com>
2025-01-19 19:29:15 +0800

    sync

--- helper.py
+++ helper.py
@@ -6,7 +6,7 @@ formatted_date = now.strftime('%A, %B %-d, %Y, at %I:%M:%S %p')
 
 with open(html_file_path, 'r+') as file:
     html_content = file.read()
-    updated_stamp = f'<!--LAST_UPDATE_START-->\n      Last updated on {formatted_date}.\n      '
+    updated_stamp = f'<!--LAST_UPDATE_START-->\n                          Last updated on {formatted_date}.\n                          '
 
     html_content = html_content.replace(
         html_content[html_content.find('<!--LAST_UPDATE_START-->'):html_content.find('<!--LAST_UPDATE_END-->')],
