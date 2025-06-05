14f0e1c8b89d37617f165e9e49544d152ba0a2f1
Yaroslav Ponomarenko <ya.ponomarenko@me.com>
2025-06-06 00:08:30 +0400

    sync

--- helper.py
+++ helper.py
@@ -9,11 +9,7 @@ with open(html_file_path, "r+") as file:
     updated_stamp = f"<!--TIME_STAMP_START-->\n          Last updated on {formatted_date}.\n          "
 
     html_content = html_content.replace(
-        html_content[
-            html_content.find("<!--TIME_STAMP_START-->") : html_content.find(
-                "<!--TIME_STAMP_END-->"
-            )
-        ],
+        html_content[html_content.find("<!--TIME_STAMP_START-->") : html_content.find("<!--TIME_STAMP_END-->")],
         updated_stamp,
     )
 
